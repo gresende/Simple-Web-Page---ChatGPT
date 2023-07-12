772699e80c5cb3243cdb2eae2514753be1585c85
Gabriel A. de Resende <resendemail@gmail.com>
2023-07-12 03:14:25 +0000

    Some bug fixes - product page and show password

--- scripts.js
+++ scripts.js
@@ -2,7 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
   const loginForm = document.getElementById('loginForm');
   const emailInput = document.getElementById('email');
   const passwordInput = document.getElementById('password');
-  const showPasswordCheckbox = document.getElementById('showPassword');
+  const viewPasswordIcon = document.getElementById('viewPasswordIcon');
+  const forgotPasswordLink = document.querySelector('.forgot-password-link');
+
+
 
   loginForm.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -25,8 +28,36 @@ document.addEventListener('DOMContentLoaded', function() {
     window.location.href = 'products-list.html';
   });
 
-  showPasswordCheckbox.addEventListener('change', function() {
-    passwordInput.type = this.checked ? 'text' : 'password';
+  emailInput.addEventListener('input', function() {
+    const email = emailInput.value;
+    const isValidEmail = validateEmail(email);
+
+    if (isValidEmail) {
+      emailInput.classList.remove('error');
+      document.getElementById('emailError').textContent = '';
+    } else {
+      emailInput.classList.add('error');
+      document.getElementById('emailError').textContent = 'Please enter a valid email address.';
+    }
+  });
+
+  viewPasswordIcon.addEventListener('click', function() {
+    const passwordType = passwordInput.getAttribute('type');
+    if (passwordType === 'password') {
+      passwordInput.setAttribute('type', 'text');
+      viewPasswordIcon.classList.remove('fa-eye');
+      viewPasswordIcon.classList.add('fa-eye-slash');
+    } else {
+      passwordInput.setAttribute('type', 'password');
+      viewPasswordIcon.classList.remove('fa-eye-slash');
+      viewPasswordIcon.classList.add('fa-eye');
+    }
+  });
+
+  forgotPasswordLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    // Add logic to navigate to the Forgot Password page
+    window.location.href = 'forgot-password.html';
   });
 
   function validateEmail(email) {
